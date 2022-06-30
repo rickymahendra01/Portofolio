494b16753cd69ee136b471d2863f42b2567ad7c1
Ricky Mahendra <rickymahendra465@gmai.com>
2022-06-30 13:49:53 +0800

    mengubah judul

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -72,9 +72,9 @@
 				<div class="row">
 					<div class="col-lg-7">
 						<div class="banner_content">
-							<h3 class="text-uppercase">Hell0</h3>
-							<h1 class="text-uppercase">Saya Ricky Mahendra</h1>
-							<h5 class="text-uppercase">senior wordpress developer</h5>
+							<h3 class="text-uppercase">Saya</h3>
+							<h1 class="text-uppercase">Ricky Mahendra</h1>
+							<h5 class="text-uppercase">web developer</h5>
 							<div class="d-flex align-items-center">
 								<a class="primary_btn" href="#"><span>Hire Me</span></a>
 								<a class="primary_btn tr-bg" href="#"><span>Get CV</span></a>
